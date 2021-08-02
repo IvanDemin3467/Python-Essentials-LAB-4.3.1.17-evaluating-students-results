e0f76095fd44b880c064ac6e4247c51245c68061
IvanDemin3467 <85740662+IvanDemin3467@users.noreply.github.com>
2021-08-02 09:13:32 +0600

    Code cleaned

--- evaluating-students-results.py
+++ evaluating-students-results.py
@@ -11,7 +11,7 @@ class StudentsDataException(Exception):
 
 
 class BadLine(StudentsDataException):
-    # Write your code here.
+    # This exception is raised in many cases if file contains bad lines
     def __init__(self, message = ""):
         StudentsDataException.__init__(self)
         print("Bad line in file. ", message)
@@ -19,7 +19,7 @@ class BadLine(StudentsDataException):
 
 
 class FileEmpty(StudentsDataException):
-    # Write your code here.
+    # This exception is raised in a case of empty file
     def __init__(self):
         StudentsDataException.__init__(self)
         print("File empty")
@@ -27,26 +27,25 @@ class FileEmpty(StudentsDataException):
 
 
 def user_input():
-    # This function prompts user to input name of file
+    # This function prompts user to input the name of the file
     while True:
         try:
-            #ui = input("Input name of file to process: ")
-            #s = open(ui, "rt")
-            s = open("Jekyll.txt", "rt")
+            s = open(input("Enter student's data filename: "), "rt")
             return s
         except IOError as e:
             print("I/O error occurred: ", strerror(e.errno)) 
 
 
 def evaluate_file(stream):
+    # This function evaluates the data in the file and converts it into list
     try:
         line_count = 0      # Store number of lines
         data_structure = [] # Store valuable data from file
         print("Data from file:")
         for line in stream: # Iterate through lines in file
+            line_count += 1 # Do not forget to count lines in a file
             
             # Evaluate lines in a file and print them to cmd
-            line_count += 1
             if len(line) == 1:
                 raise BadLine("Line is empty")
             print(line, end = "")
@@ -66,8 +65,9 @@ def evaluate_file(stream):
                 except:
                     BadLine("Incorrect grade in line " + str(line_count))
                     continue
-            data_structure.append(entries) # save data to structure
+            data_structure.append(entries) # save data to list
 
+        # Evaluate the length of the file
         if line_count == 0:
             raise FileEmpty()
 
@@ -82,8 +82,12 @@ def evaluate_file(stream):
 
 
 def students_results():
-    # 
-    # File open is here
+    # This function processes results stored in a list;
+    # perfoms grades summation;
+    # converts results into a dict;
+    # and prints report to cdm
+    
+    # Read data from file
     stream = user_input()
     data_structure = evaluate_file(stream)
 
